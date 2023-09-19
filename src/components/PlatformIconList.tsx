63a72744a8b5eb4586f107c1286c8a0fe8209620
Yossi Sh <yossi9980@gmail.com>
2023-09-19 14:48:27 +0300

    display Icons

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,44 @@
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { Platform } from "../hooks/useGames";
+import { HStack, Icon, Text } from "@chakra-ui/react";
+import { IconType } from "react-icons";
+import { color } from "framer-motion";
+
+interface Props {
+  platforms: Platform[];
+}
+
+const PlatformIconList = ({ platforms }: Props) => {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    linux: FaLinux,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+    android: FaAndroid,
+  };
+  return (
+    <>
+      <HStack marginY={1}>
+        {platforms.map((Platform) => (
+          <Icon as={iconMap[Platform.slug]} color={"gray.500"} />
+        ))}
+      </HStack>
+    </>
+  );
+};
+
+export default PlatformIconList;
