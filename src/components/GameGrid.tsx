32437b155c0dfca125c0f31c92fcc912cbeaa6cc
Yossi Sh <yossi9980@gmail.com>
2023-09-19 16:21:04 +0300

    Create A Generic Data Feching hook

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -5,14 +5,14 @@ import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
 
 const GameGrid = () => {
-  const { games, error, isLoading } = useGames();
+  const { data, error, isLoading } = useGames();
   const Skeleton = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
   return (
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid
         padding={10}
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        columns={{ sm: 1, md: 1, lg: 2, xl: 3 }}
         spacing={10}
       >
         {isLoading &&
@@ -21,7 +21,7 @@ const GameGrid = () => {
               <GameCardSkeleton key={skeleton} />{" "}
             </GameCardContainer>
           ))}
-        {games.map((game) => (
+        {data.map((game) => (
           <GameCardContainer>
             <GameCard key={game.id} game={game} />{" "}
           </GameCardContainer>
