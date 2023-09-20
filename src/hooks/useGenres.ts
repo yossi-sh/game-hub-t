676f7540036ed02990a142fbc4b3b01b576d4ecd
Yossi Sh <yossi9980@gmail.com>
2023-09-20 14:39:43 +0300

    Update Title

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,11 +1,10 @@
-import genres from "../Data/genres";
-import useData from "./useData";
+import genres from "../data/genres";
 
 
 export interface Genre{
     id:number
     name:string
-    image_backGround : string;
+    image_background : string;
 }
 
 const useGenres = () => ({data:genres , error:null , isLoading : false})
