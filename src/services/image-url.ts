64c1acfc43538daef3ddfc5c515a52e0bb961594
Yossi Sh <yossi9980@gmail.com>
2023-09-20 10:53:21 +0300

    Handle Games WithNo Image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,8 @@
+import noImage from '../assets/no-image-placeholder.webp';
+
 const getCroppedImageUrl = (url:string) =>{
-    if(!url) return ('')
+    if(!url) return noImage;
+
     const index = url.indexOf('media/') + 'media/'.length;
    return url.slice(0,index) + 'crop/600/400/' + url.slice(index);
 }
